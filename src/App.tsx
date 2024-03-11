9c9612f67918a5ce9b309f1cfe753c186c299add
raviteja83 <ravi.theja@100ms.live>
2024-03-11 19:45:36 +0530

    feat: take room codes in the url

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
 import { HMSRoom } from "./HMSRoom";
 
+
+const useSearchParams = (param: string) => {
+  const  searchParams  = new URLSearchParams(window.location.search);
+
+  return searchParams.get(param);
+}
+
 function App() {
+  const roomCodes = useSearchParams('roomCodes');
+
+  if(!roomCodes) {
+    return <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+       Please provide &nbsp;<b>comma(,)</b>&nbsp;separated roomCodes in search e.g. ?roomCode=code1,code2
+    </div>
+  }
   return (
     <div
       style={{
@@ -12,8 +26,7 @@ function App() {
         height: "100%",
       }}
     >
-      <HMSRoom roomCode="kqu-fxgs-qtb" userId="user1" />
-      <HMSRoom roomCode="xqk-nljs-pnv" userId="user2" />
+      {roomCodes?.split(',').map((roomCode, index) => <HMSRoom roomCode={roomCode} userId={`user-${index}`} />)}
     </div>
   );
 }
