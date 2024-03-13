e0c2e2415490b0de64c4976bb1e2182436eab83f
raviteja83 <ravi.theja@100ms.live>
2024-03-13 15:55:54 +0530

    feat: link to qa

--- src/HMSRoom.tsx
+++ src/HMSRoom.tsx
@@ -10,12 +10,13 @@ const AuthToken = ({ roomCode, userId }: { roomCode: string; userId: string }) =
       return;
     }
     actions
-      .getAuthTokenByRoomCode({ roomCode, userId })
+      .getAuthTokenByRoomCode({ roomCode, userId }, { endpoint: 'https://auth-nonprod.100ms.live/v2/token' })
       .then(token => {
         actions
           .join({
             authToken: token,
             userName: userId,
+            initEndpoint: 'https://qa-in2-ipv6.100ms.live/init',
           })
           .catch(console.error);
       })
