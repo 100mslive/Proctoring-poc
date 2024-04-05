1752d5bf397994de676cb9b9d3425ed90c051255
Kaustubh Kumar <kaustubhkumar05@gmail.com>
2024-04-05 12:15:27 +0530

    feat: display search param usage on home page (#1)

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export const getRoomCodes = () => {
   const searchParams = new URLSearchParams(window.location.search);
-  return (searchParams.get('roomCodes') || '').split(',');
+  return searchParams.get('roomCodes')?.split(',') || [];
 };
 
 export const hasVideoTrack = peer => {
